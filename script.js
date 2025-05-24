2600c86a7198baab3b084596d2e39437f5a2329f
Nguyen Cong Viet <vietlp1000@gmail.com>
2025-05-25 03:07:13 +0700

    feat: responsive

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuIcon = document.getElementById("menuIcon");
+const menuNav = document.getElementById("menuNav");
+
+menuIcon.addEventListener("click", (e) => {
+    e.stopPropagation();
+    menuNav.classList.toggle("show");
+});
+
+menuNav.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+document.addEventListener('click', () => {
+    if (menuNav.classList.contains('show')) {
+        menuNav.classList.remove('show');
+    }
+});
