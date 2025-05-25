036b6720869234acd2942116164307d0dbf1bbc4
Nguyen Cong Viet <vietlp1000@gmail.com>
2025-05-25 23:28:02 +0700

    feat: delete file h.html

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const menuIcon = document.getElementById("menuIcon");
 const menuNav = document.getElementById("menuNav");
 
 menuIcon.addEventListener("click", (e) => {
-    e.stopPropagation();
-    menuNav.classList.toggle("show");
+  e.stopPropagation();
+  menuNav.classList.toggle("show");
 });
 
-menuNav.addEventListener('click', (e) => {
-    e.stopPropagation();
+menuNav.addEventListener("click", (e) => {
+  e.stopPropagation();
 });
 
-document.addEventListener('click', () => {
-    if (menuNav.classList.contains('show')) {
-        menuNav.classList.remove('show');
-    }
+document.addEventListener("click", () => {
+  if (menuNav.classList.contains("show")) {
+    menuNav.classList.remove("show");
+  }
 });
